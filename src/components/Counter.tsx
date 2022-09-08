65a62bd578701d8a0cec29a875d7da63466f0349
Emma Lindmark <emma_lindmark@live.se>
2022-09-08 18:16:44 +0200

    shortened the increase/decrease code and added showing of prev calculations

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,47 +3,40 @@ import '../styles/styles.css';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [tempCount, setTempCount] = useState(0);
+    const [previouslyAdded, setPrevoiuslyAdded] = useState<number[]>([]);
 
-    const increase1 = () => {
-        setCount(count + 1);
-    }
-    const increase5 = () => {
-        setCount(count + 5);
-    }
-    const increase10 = () => {
-        setCount(count + 10);
-    }
-
-    const decrease1 = () => {
-        setCount(count - 1);
-    }
-    const decrease5 = () => {
-        setCount(count - 5);
-    }
-    const decrease10 = () => {
-        setCount(count - 10);
+    const changeCount = (count: number) => {
+        return () => setTempCount(tempCount + count);
     }
 
     const addCalculation = () => {
-
+        setCount(count + tempCount);
+        setPrevoiuslyAdded([...previouslyAdded, tempCount]);
+        setTempCount(0);
+    }
+    const showPrevCalculations = () => {
+        return previouslyAdded.slice(-5).map((count, index) => ((index ? ',' : '') + count));
     }
 
     return (
         <div className="Counter">
             <div className='container'>
                 <div className='row'>
-                    <button onClick={increase1} className='countButton'> +1</button>
-                    <button onClick={increase5} className='countButton'> +5</button>
-                    <button onClick={increase10} className='countButton'> +10</button>
+                    <button onClick={changeCount(1)} className='countButton'> +1</button>
+                    <button onClick={changeCount(5)} className='countButton'> +5</button>
+                    <button onClick={changeCount(10)} className='countButton'> +10</button>
                 </div>
                 <div>
                     <p className='text'> Current count is: {count}</p>
+                    <button onClick={addCalculation}>add {tempCount} to current count</button>
                 </div>
                 <div>
-                    <button onClick={decrease1} className='countButton'>-1</button>
-                    <button onClick={decrease5} className='countButton'>-5</button>
-                    <button onClick={decrease10} className='countButton'>-10</button>
+                    <button onClick={changeCount(-1)} className='countButton'>-1</button>
+                    <button onClick={changeCount(-5)} className='countButton'>-5</button>
+                    <button onClick={changeCount(-10)} className='countButton'>-10</button>
                 </div>
+                <div>{showPrevCalculations()}</div>
             </div>
         </div>
     );
